58b2a72100077e28421a40c58c81149e0917bd50
hakshu <flyhighup.air25@gmail.com>
2023-04-19 18:09:16 +0900

    feat: GA追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
-import partytown from "@astrojs/partytown";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()]
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
